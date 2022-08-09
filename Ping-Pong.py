6f27784f3a9ae2d35b72d365062596dbeb8fa07b
ShaxzodY <110611612+ShaxzodY@users.noreply.github.com>
2022-08-09 19:06:49 +0500

    Update Ping-Pong.py
    v1.1

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -1,3 +1,6 @@
+
+
+from cgitb import reset
 from pygame import *
 from random import randint
 from time import time as timer
@@ -26,7 +29,7 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
         self.speed = speed
     def reset(self):
-        win.blit(self.image, (self.rect.x, self.rect.y))
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
     def update_1(self):
@@ -34,11 +37,19 @@ class Player(GameSprite):
     def update_r(self):
         pass
 
+racket1 = Player('object3.jpg', 5, 200, 30, 60, 10)
+racket2 = Player('object4.jpg', 650, 300, 30, 60, 10)
+ball = GameSprite('object5.png', 100, 300, 50, 50, 10)
+
+
 
 finish = False
 run = True
 while run:
     window.blit(background, (0, 0))
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
     for e in event.get():
         if e.type == QUIT:
             run = False
